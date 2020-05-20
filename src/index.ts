c73971563613d4f3f6bc112bcb41dfd63fbaefbb
pooya parsa <pooya@pi0.ir>
2020-05-20 23:07:27 +0200

    perf: don't use object lookup

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,7 @@ export type JSONValue = true | false | null | string | Number | JSONObject | JSO
 const suspectProtoRx = /"(?:_|\\u005[Ff])(?:_|\\u005[Ff])(?:p|\\u0070)(?:r|\\u0072)(?:o|\\u006[Ff])(?:t|\\u0074)(?:o|\\u006[Ff])(?:_|\\u005[Ff])(?:_|\\u005[Ff])"\s*:/
 const suspectConstructorRx = /"(?:c|\\u0063)(?:o|\\u006[Ff])(?:n|\\u006[Ee])(?:s|\\u0073)(?:t|\\u0074)(?:r|\\u0072)(?:u|\\u0075)(?:c|\\u0063)(?:t|\\u0074)(?:o|\\u006[Ff])(?:r|\\u0072)"\s*:/
 
-const STR_MAP = {
-  true: true,
-  false: false,
-  null: null,
-  nan: NaN,
-  infinity: Infinity,
-  undefined
-}
+const STR_MAP = new Map()
 
 function jsonParseTransform (key: string, value: any): any {
   if (key === '__proto__' || key === 'prototype' || key === 'constructor') {
@@ -28,10 +21,12 @@ export default function destr (val: string | any): JSONValue | undefined {
   }
 
   const _lval = val.toLowerCase()
-  if (_lval in STR_MAP) {
-    // @ts-ignore
-    return STR_MAP[_lval]
-  }
+  if (_lval === 'true') { return true }
+  if (_lval === 'false') { return false }
+  if (_lval === 'null') { return null }
+  if (_lval === 'nan') { return NaN }
+  if (_lval === 'infinity') { return Infinity }
+  if (_lval === 'undefined') { return undefined }
 
   if (val[0] !== '"' && val[0] !== '{' && val[0] !== '[') {
     return val
