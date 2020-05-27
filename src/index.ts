49bcd9f7529e9289b987eb7aa98ff45148d2585d
pooya parsa <pooya@pi0.ir>
2020-05-27 11:41:51 +0200

    perf: simplify jsonSig regex

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export type JSONValue = true | false | null | string | Number | JSONObject | JSO
 const suspectProtoRx = /"(?:_|\\u005[Ff])(?:_|\\u005[Ff])(?:p|\\u0070)(?:r|\\u0072)(?:o|\\u006[Ff])(?:t|\\u0074)(?:o|\\u006[Ff])(?:_|\\u005[Ff])(?:_|\\u005[Ff])"\s*:/
 const suspectConstructorRx = /"(?:c|\\u0063)(?:o|\\u006[Ff])(?:n|\\u006[Ee])(?:s|\\u0073)(?:t|\\u0074)(?:r|\\u0072)(?:u|\\u0075)(?:c|\\u0063)(?:t|\\u0074)(?:o|\\u006[Ff])(?:r|\\u0072)"\s*:/
 
-const JsonSigRx = /^["{[].*|^-?[0-9][0-9.]+$/
+const JsonSigRx = /^["{[]|^-?[0-9][0-9.]+$/
 
 function jsonParseTransform (key: string, value: any): any {
   if (key === '__proto__' || key === 'constructor') {
